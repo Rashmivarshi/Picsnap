fffb9fc221249a51df643559a92fac2cd80bd556
Rashmivarshi <rashmimaniampare@gmail.com>
2024-05-24 10:31:35 +0530

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans} from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 import { ClerkProvider} from "@clerk/nextjs";
 
 const IBMPlex = IBM_Plex_Sans({ 
